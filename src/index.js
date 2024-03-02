ae9cf5c9971cce986d55591cddb3f412aa5a995f
Ikerdiazmontilla <ikercorrevuela@gmail.com>
2024-03-02 16:17:00 -0600

    Create functional merge sort

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.css';
+
+const mergeSort = function (array) {
+  const sortedArray = [];
+  if (array.length === 1) {
+    return array;
+  }
+  const mArrayIndex = parseInt(array.length / 2, 10);
+  const lArrayN = array.slice(0, mArrayIndex);
+  const lArray = mergeSort(lArrayN);
+
+  const rArrayN = array.slice(mArrayIndex, array.length);
+  const rArray = mergeSort(rArrayN);
+
+  while (lArray.length > 0 || rArray.length > 0) {
+    if (rArray.length === 0) {
+      for (item of lArray) {
+        sortedArray.push(lArray.shift());
+      }
+    } else if (rArray.length === 0) {
+      for (item of rArray) {
+        sortedArray.push(rArray.shift());
+      }
+    } else if (lArray[0] < rArray[0]) {
+      sortedArray.push(lArray.shift());
+    } else {
+      sortedArray.push(rArray.shift());
+    }
+  }
+  return sortedArray;
+};
+
+const arr = [105, 79, 100, 110];
+console.log(mergeSort(arr));
